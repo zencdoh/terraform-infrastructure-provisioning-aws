3bc19d5fb063e38e1489c238070b3ff5d699da84
pratiksha94 <pratiksha.khodade@gmail.com>
2018-09-20 18:14:47 +0000

    terraform files

---
+++ terraform.sh
@@ -0,0 +1,7 @@
+export TF_VAR_name="default-vpc"
+export TF_VAR_cidr="10.0.0.0/24"
+export TF_VAR_azs='["us-east-1a"]'
+export TF_VAR_private_subnets='["10.0.0.0/25"]'
+export TF_VAR_public_subnets='["10.0.0.128/25"]'
+export TF_VAR_default_ami="ami-759bc50a"
+export TF_VAR_key_name="Demo"
